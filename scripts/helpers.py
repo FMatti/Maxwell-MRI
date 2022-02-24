67fc4028bd21253435500850f67cfbfe04953a60
FMatti <somecallmefabio@gmail.com>
2022-02-24 15:56:37 +0100

    implement proposed adjustments

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -5,19 +5,19 @@ helpers.py
 Collection of helper functions. 
 """
 
-from fenics import *
-from mshr import *
+import fenics as fen
+import mshr
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.sparse import coo_matrix, csr_matrix
+import scipy.sparse
 
 def export_form_as_sparse_matrix(a, fname):
     """
     Export form 'a' as sparse matrix in the format [i, j, a(i, j)].
     """
-    M = assemble(a)
-    Mmat = as_backend_type(M).mat()
-    mat = coo_matrix(csr_matrix(Mmat.getValuesCSR()[::-1]))
+    M = fen.assemble(a)
+    Mmat = fen.as_backend_type(M).mat()
+    mat = scipy.sparse.coo_matrix(scipy.sparse.csr_matrix(Mmat.getValuesCSR()[::-1]))
 
     np.savetxt(fname + '.txt', np.c_[mat.row, mat.col, mat.data],
                fmt=['%d', '%d', '%.16f']) 
@@ -35,5 +35,6 @@ def export_field_at_vertex_coordinates(A, mesh, fname):
     np.savetxt(fname + '.txt', np.c_[Acoords, coords], fmt = '%.16f')
 
 def export_field_as_png_plot(A, fname):
-    plt.colorbar(plot(A))
+    plt.figure()
+    plt.colorbar(fen.plot(A))
     plt.savefig(fname + '.png', dpi=100)
