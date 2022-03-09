de184b6f20967ee3876609ff8e123c2265df8d36
FMatti <somecallmefabio@gmail.com>
2022-03-09 15:25:40 +0100

    start with frequency analysis of 3D waveguides

--- scripts/wg3D.py
+++ scripts/wg3D.py
@@ -1,5 +1,5 @@
 """
-wg2D.py
+wg3D.py
 -------
 
 A waveguide is a medium separated from its environment by
@@ -13,6 +13,7 @@ import helpers
 import numpy as np
 
 def create(mesh, inlet):
+    
     # Create a boundary function evaluating to 1 for inlet and 2 for wall
     boundary = fen.MeshFunction('size_t', mesh, mesh.topology().dim() - 1)
     inlet.mark(boundary, 1)
@@ -21,29 +22,29 @@ def create(mesh, inlet):
             return on_boundary and not inlet.inside(x, 'on_boundary')
     Wall().mark(boundary, 2)
     
-    V = fen.FunctionSpace(mesh, 'P', 1)
+    V = fen.FunctionSpace(mesh, 'N1curl', 1)
     
     # Dirichlet boundary condition
-    bc = fen.DirichletBC(V, fen.Constant('0.0'), boundary, 2)
+    bc = fen.DirichletBC(V, fen.Constant(('0.0', '0.0', '0.0')), boundary, 2)
     
     return boundary, V, bc
 
-def setup(boundary, g_z_inlet, mu, eps, V):    
-    A_z = fen.TrialFunction(V)
-    v_z = fen.TestFunction(V)
+def setup(boundary, g_inlet, mu, eps, V):    
+    A = fen.TrialFunction(V)
+    v = fen.TestFunction(V)
 
     # Neumann boundary condition for linear form
     ds = fen.Measure('ds', subdomain_data=boundary)
-    L = g_z_inlet*v_z*ds(1)
+    L = fen.dot(g_inlet, v)*ds(1)
 
     # Neumann boundary condition for bilinear form
-    K = (1 / mu)*fen.dot(fen.grad(A_z), fen.grad(v_z))*fen.dx
-    M = eps*A_z*v_z*fen.dx
+    K = (1 / mu)*fen.dot(fen.curl(A), fen.curl(v))*fen.dx
+    M = eps*fen.dot(A, v)*fen.dx
     
     return K, M, L
 
 def solve(omega, K, M, L, bc, V):  
     a = K - omega**2 * M  
-    A_z = fen.Function(V)
-    fen.solve(a == L, A_z, bc)
-    return A_z
+    A = fen.Function(V)
+    fen.solve(a == L, A, bc)
+    return A
