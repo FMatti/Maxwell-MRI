54ea5a57b4381d912376d849da9f1d315a1c5e00
FMatti <somecallmefabio@gmail.com>
2022-04-13 10:38:13 +0200

    patch up circular waveguide

--- src/vector_space.py
+++ src/vector_space.py
@@ -52,10 +52,10 @@ class VectorSpaceL2(VectorSpace):
             mesh = THMP.get_V().mesh()
             indicator = fen.MeshFunction('size_t', mesh, mesh.topology().dim() - 1)
             indicator.set_all(0)
-            trace.mark(indicator, 1)
+            self.trace.mark(indicator, 1)
             ds = fen.Measure('ds', subdomain_data=indicator)
             form = fen.assemble(fen.dot(u, v)* ds(1))
             coords = THMP.V.tabulate_dof_coordinates()
-            is_on_trace = lambda x: trace.inside(x, 'on_boundary')
+            is_on_trace = lambda x: self.trace.inside(x, 'on_boundary')
             on_trace = np.apply_along_axis(is_on_trace, 1, coords)
             self.matrix = THMP.tosparse(form)[on_trace, :][:, on_trace]
