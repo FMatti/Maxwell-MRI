8c6f0d4f0d9ca676678bc1e07a134c5d70bae398
FMatti <somecallmefabio@gmail.com>
2022-04-28 07:48:56 +0200

    simplify plotting

--- src/helpers.py
+++ src/helpers.py
@@ -7,8 +7,7 @@ def plot_solution_norms(ax, THMP, VS, **kwargs):
     omegas = THMP.get_frequency()
     norms = np.empty(len(omegas))
     A = THMP.get_solution(tonumpy=True, trace=VS.get_trace())
-    for i, omega in enumerate(omegas):
-        norms[i] = VS.norm(A[i])
+    norms = [VS.norm(a) for a in A]
     ax.plot(omegas, norms, **kwargs)
     ax.set_yscale('log')
 
@@ -18,8 +17,6 @@ def plot_surrogate_norms(ax, MRI, VS, a=None, b=None, N=1000, **kwargs):
         a = np.min(MRI.RI.get_nodes())
     if b is None:
         b = np.max(MRI.RI.get_nodes())
-    norms = np.empty(N)
-    RI = MRI.get_surrogate()
     linspace = np.linspace(a, b, N)
     ax.plot(linspace, [VS.norm(MRI.RI(x)) for x in linspace], **kwargs)
     ax.set_yscale('log')
